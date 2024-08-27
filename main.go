f5dcb828e07464452b836ee2679b681332fd11ad
lhdht <15229684090@163.com>
2024-08-27 11:01:23 +0800

    fix: 修改入口番薯

--- main.go
+++ main.go
@@ -4,26 +4,9 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 package main
 
 import (
-	"fmt"
-	"github.com/fireflycore/cli/pkg"
+	"github.com/fireflycore/cli/cmd"
 )
 
-var ask = []string{
-	"Please input your project name.",
-	"Please choose your development language.",
-}
-
 func main() {
-	//cmd.Execute()
-	//ip := pkg.NewInput(ask[0])
-	//if _, err := ip.Run(); err != nil {
-	//	fmt.Println(err)
-	//	return
-	//}
-
-	sp := pkg.NewSelect(ask[1], pkg.LANGUAGE)
-	if _, err := sp.Run(); err != nil {
-		fmt.Println(err)
-		return
-	}
+	cmd.Execute()
 }
