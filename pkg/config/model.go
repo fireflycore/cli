47acee6f79cac3d4e75b33f5b06e1ab28aab8265
lhdht <15229684090@163.com>
2024-08-31 22:42:14 +0800

    feature: 缓存

---
+++ pkg/config/model.go
@@ -0,0 +1,12 @@
+package config
+
+type CacheConfigEntity struct {
+	Root     string `json:"root" yaml:"root" mapstructure:"root"`
+	Config   string `json:"config" yaml:"config" mapstructure:"config"`
+	Template string `json:"template" yaml:"template" mapstructure:"template"`
+}
+
+type CoreEntity struct {
+	Cache   CacheConfigEntity `json:"cache" yaml:"cache" mapstructure:"cache"`
+	Version map[string]string `json:"version" yaml:"version" mapstructure:"version"`
+}
