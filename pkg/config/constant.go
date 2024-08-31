dee39192e099a7a2020c095f8614a1acf2d8f28c
lhdht <15229684090@163.com>
2024-08-31 22:43:37 +0800

    feature: 常量配置

---
+++ pkg/config/constant.go
@@ -0,0 +1,22 @@
+package config
+
+const CLI_NAME = "firefly"
+const CLI_CONFIG_FILE_NAME = "cli"
+const CLI_CONFIG_FILE_TYPE = "yaml"
+
+const REPO_OWNER = "lhdhtrc"
+
+var Language = []string{
+	"Go",
+	//"Rust",
+	//"Dart",
+	//"Swift",
+	//"Kotlin",
+	//"Python",
+	//"Node.js",
+	//"Java",
+	//"PHP",
+	//"C++",
+	//"C#",
+	//"Ruby",
+}
