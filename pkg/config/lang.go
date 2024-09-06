21e4a7bfee0ba296571c8ac368058516fd19b4d6
lhdht <15229684090@163.com>
2024-09-06 08:41:04 +0800

    feature: 增加多语言配置

---
+++ pkg/config/lang.go
@@ -0,0 +1,25 @@
+package config
+
+var CreateProblemTextLang = map[string][]string{
+	"zh": {
+		"请输入项目名称.",
+		"请选择开发语言.",
+		"请选择数据库.",
+	},
+	"en": {
+		"Please input your project name.",
+		"Please choose your development language.",
+		"Please select the database you want.",
+	},
+}
+
+var TipsTextLang = map[string][]string{
+	"zh": {
+		"ctrl+c或q退出cli.",
+		"回车确认或下一步.",
+	},
+	"en": {
+		"ctrl+c or q to exit the cli.",
+		"enter confirm or next step.",
+	},
+}
