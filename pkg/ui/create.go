a4b6b5bad4d3551191e28f84f188d6c4de1dba10
lhdht <15229684090@163.com>
2024-09-07 14:30:35 +0800

    fix: 废弃数据库选择

--- pkg/ui/create.go
+++ pkg/ui/create.go
@@ -5,6 +5,7 @@ import (
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/fireflycore/cli/pkg/config"
+	"github.com/fireflycore/cli/pkg/store"
 	"regexp"
 	"strings"
 )
@@ -14,11 +15,10 @@ var inputReg = regexp.MustCompile("[^a-zA-Z0-9_]+")
 type CreateFormModelEntity struct {
 	Config *ConfigEntity
 
-	askIndex      int
+	problemIndex  int
 	languageIndex int
-	databaseFocus int
 
-	projectInput textinput.Model
+	input textinput.Model
 }
 
 func (model CreateFormModelEntity) Init() tea.Cmd {
@@ -34,46 +34,31 @@ func (model CreateFormModelEntity) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		case "q", "ctrl+c":
 			return model, tea.Quit // 退出程序
 		case "up":
-			if model.askIndex == 1 && model.languageIndex > 0 {
+			if model.problemIndex == 1 && model.languageIndex > 0 {
 				model.languageIndex--
 			}
-			if model.askIndex == 2 && model.databaseFocus > 0 {
-				model.databaseFocus--
-			}
 		case "down":
-			if model.askIndex == 1 && (model.languageIndex < len(config.LANGUAGE)-1) {
+			if model.problemIndex == 1 && (model.languageIndex < len(config.LANGUAGE)-1) {
 				model.languageIndex++
 			}
-			if model.askIndex == 2 && (model.databaseFocus < len(DatabaseList)-1) {
-				model.databaseFocus++
-			}
 		case "enter":
-			switch model.askIndex {
+			switch model.problemIndex {
 			case 0:
-				input := model.projectInput.Value()
+				input := model.input.Value()
 				model.Config.Project = inputReg.ReplaceAllString(input, "")
-				model.askIndex++
+				model.problemIndex++
 			case 1:
 				model.Config.Language = strings.ToLower(config.LANGUAGE[model.languageIndex])
-				if list, ok := Database[model.Config.Language]; ok {
-					DatabaseList = list
-				}
-				model.askIndex++
-			case 2:
-				model.askIndex++
+				model.problemIndex++
 			}
-			if model.askIndex+1 > len(CREATE_PROBLEM) {
+			if model.problemIndex+1 > len(config.CREATE_PROBLEM) {
 				return model, tea.Quit
-			}
-		case " ":
-			if model.askIndex == 2 {
-				DatabaseList[model.databaseFocus].Select = !DatabaseList[model.databaseFocus].Select
 			}
 		}
 	}
 
-	if model.askIndex == 0 {
-		model.projectInput, cmd = model.projectInput.Update(msg)
+	if model.problemIndex == 0 {
+		model.input, cmd = model.input.Update(msg)
 	}
 
 	return model, cmd
@@ -82,86 +67,59 @@ func (model CreateFormModelEntity) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 func (model CreateFormModelEntity) View() string {
 	var str strings.Builder
 
+	prefix := config.Color.Primary.Render("<-")
+	problem := config.CREATE_PROBLEM[store.Use.Config.Global.TextLanguage]
 	// 处理第一个问题（如果尚未回答）
-	if model.askIndex == 0 && len(model.Config.Project) == 0 {
-		str.WriteString(fmt.Sprintf("%s %s\n", primary.Render("<-"), info.Render(CREATE_PROBLEM[0])))
-		str.WriteString(fmt.Sprintf("-> %s\n", model.projectInput.View()))
+	if model.problemIndex == 0 && len(model.Config.Project) == 0 {
+		str.WriteString(fmt.Sprintf("%s %s\n", prefix, config.Color.Info.Render(problem[0])))
+		str.WriteString(fmt.Sprintf("-> %s\n", model.input.View()))
 	}
 
 	// 处理第二个问题（如果尚未回答）
-	if model.askIndex == 1 && len(config.LANGUAGE) == 0 {
-		str.WriteString(fmt.Sprintf("%s %s %s\n", primary.Render("<-"), info.Render(fmt.Sprintf("%s -", CREATE_PROBLEM[0])), primary.Render(model.Config.Project)))
-		str.WriteString(fmt.Sprintf("%s %s\n", primary.Render("<-"), info.Render(CREATE_PROBLEM[1])))
+	if model.problemIndex == 1 && len(model.Config.Language) == 0 {
+		str.WriteString(fmt.Sprintf("%s %s %s\n", prefix, config.Color.Info.Render(fmt.Sprintf("%s -", problem[0])), config.Color.Primary.Render(model.Config.Project)))
+		str.WriteString(fmt.Sprintf("%s %s\n", prefix, config.Color.Info.Render(problem[1])))
 
 		for ii, item := range config.LANGUAGE {
 			selected := "  "
 			if model.languageIndex == ii {
-				selected = focus.Render("->")
-				item = focus.Render(item)
+				selected = config.Color.Focus.Render("->")
+				item = config.Color.Focus.Render(item)
 			}
 			str.WriteString(fmt.Sprintf("%s %s\n", selected, item))
 		}
 	}
 
-	// 处理第三个问题（如果尚未回答）
-	if model.askIndex == 2 && len(model.Config.Database) == 0 {
-		str.WriteString(fmt.Sprintf("%s %s %s\n", primary.Render("<-"), info.Render(fmt.Sprintf("%s -", CREATE_PROBLEM[0])), primary.Render(model.Config.Project)))
-		str.WriteString(fmt.Sprintf("%s %s %s\n", primary.Render("<-"), info.Render(fmt.Sprintf("%s -", CREATE_PROBLEM[1])), primary.Render(model.Config.Language)))
-		str.WriteString(fmt.Sprintf("%s %s\n", primary.Render("<-"), info.Render(CREATE_PROBLEM[2])))
-
-		for ii, item := range DatabaseList {
-			selected := "[ ]"
-			value := item.Type
-
-			if model.databaseFocus == ii && item.Select {
-				selected = info.Render("[*]")
-				value = info.Render(value)
-			} else if model.databaseFocus == ii {
-				selected = info.Render(selected)
-				value = info.Render(value)
-			} else if item.Select {
-				selected = focus.Render("[*]")
-				value = focus.Render(value)
-			}
-			str.WriteString(fmt.Sprintf("%s %s\n", selected, value))
-		}
-
-		if len(DatabaseList) == 0 {
-			str.WriteString("Database configuration is not implemented under this development language.\n")
-		}
-	}
-
 	// 如果已经完成了所有问题
-	if model.askIndex == len(CREATE_PROBLEM) {
-		str.WriteString(fmt.Sprintf("%s %s %s\n", primary.Render("<-"), info.Render(fmt.Sprintf("%s -", CREATE_PROBLEM[0])), primary.Render(model.Config.Project)))
-		str.WriteString(fmt.Sprintf("%s %s %s\n", primary.Render("<-"), info.Render(fmt.Sprintf("%s -", CREATE_PROBLEM[1])), primary.Render(model.Config.Language)))
-
-		var dbs []string
-		for _, item := range DatabaseList {
-			dbs = append(dbs, item.Type)
+	if model.problemIndex == len(problem) {
+		str.WriteString(fmt.Sprintf("%s %s %s\n", prefix, config.Color.Info.Render(fmt.Sprintf("%s -", problem[0])), config.Color.Primary.Render(model.Config.Project)))
+		str.WriteString(fmt.Sprintf("%s %s %s\n", prefix, config.Color.Info.Render(fmt.Sprintf("%s -", problem[1])), config.Color.Primary.Render(model.Config.Language)))
+	} else {
+		prefix = config.Color.Warning.Render("->")
+		tips := config.TIPS_TEXT[store.Use.Config.Global.TextLanguage]
+		for index, item := range tips {
+			str.WriteString(fmt.Sprintf("\n%s %s", prefix, item))
+			if index == len(tips)-1 {
+				str.WriteString("\n")
+			}
 		}
-		str.WriteString(fmt.Sprintf("%s %s %s\n", primary.Render("<-"), info.Render(fmt.Sprintf("%s -", CREATE_PROBLEM[2])), primary.Render(strings.Join(dbs, ", "))))
 	}
-
-	prefix := warning.Render("->")
-	str.WriteString(fmt.Sprintf("\n%s ctrl+c or q to exit the cli.\n%s enter confirm or next step.\n", prefix, prefix))
 
 	return str.String()
 }
 
 func NewCreate() (*ConfigEntity, error) {
-	projectInput := textinput.New()
-	projectInput.Prompt = ""
-	projectInput.Focus()
+	input := textinput.New()
+	input.Prompt = ""
+	input.Focus()
 
 	form := CreateFormModelEntity{
 		Config: &ConfigEntity{},
 
-		askIndex:      0,
+		problemIndex:  0,
 		languageIndex: 0,
-		databaseFocus: 0,
 
-		projectInput: projectInput,
+		input: input,
 	}
 
 	p := tea.NewProgram(form)
