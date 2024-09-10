a7d255aa494ec1f2afa3e09714e3abc68393d809
lhdht <15229684090@163.com>
2024-09-10 09:48:35 +0800

    feature: 添加store和module

---
+++ cmd/proto_add_module.go
@@ -0,0 +1,30 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// protoAddModuleCmd represents the protoAddModule command
+var protoAddModuleCmd = &cobra.Command{
+	Use:   "module",
+	Short: "add proto module",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("protoAddModule called")
+	},
+}
+
+func init() {
+	protoAddCmd.AddCommand(protoAddModuleCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// protoAddModuleCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// protoAddModuleCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ cmd/proto_add_store.go
@@ -0,0 +1,30 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// protoAddStoreCmd represents the protoAddStore command
+var protoAddStoreCmd = &cobra.Command{
+	Use:   "store",
+	Short: "add proto store",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("protoAddStore called")
+	},
+}
+
+func init() {
+	protoAddCmd.AddCommand(protoAddStoreCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// protoAddStoreCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// protoAddStoreCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
