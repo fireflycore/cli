02dcf071866b0b7dbe711bff68719718dc4864e2
lhdht <15229684090@163.com>
2024-09-11 08:48:10 +0800

    feature: 新增proto store and module视图问题

--- pkg/view/problem.go
+++ pkg/view/problem.go
@@ -5,6 +5,16 @@ var CREATE_PROJECT_PROBLEM = []string{
 	"Please choose your development language.",
 }
 
+var PROTO_ADD_STORE = []string{
+	"Please select a store type.",
+	"Please enter the store address.",
+}
+
+var PROTO_ADD_MODULE = []string{
+	"Please select a store address.",
+	"Please enter the module name.",
+}
+
 var TIPS_TEXT = []string{
 	"ctrl+c or q to exit the cli.",
 	"enter confirm or next step.",
