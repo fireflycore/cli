8feb994496bc549652a847cf3e92553b0ee65755
lhdht <15229684090@163.com>
2024-09-14 22:41:15 +0800

    special: 中秋快乐

--- pkg/buf/store.go
+++ pkg/buf/store.go
@@ -3,7 +3,7 @@ package buf
 import "fmt"
 
 func (gen *GenConfigEntity) AddGenStore(mode, store string) {
-	fmt.Println(mode, store)
+	fmt.Println(mode, store, "睡个好觉")
 }
 
 func (gen *GenConfigEntity) RemoveGenStore(store string) {
