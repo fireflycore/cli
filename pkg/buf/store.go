52742b29f3cd6e81abdd0b97e6493831d4c9f078
lhdht <15229684090@163.com>
2024-09-16 15:59:54 +0800

    feature: 新增proto商店

--- pkg/buf/store.go
+++ pkg/buf/store.go
@@ -2,8 +2,32 @@ package buf
 
 import "fmt"
 
-func (gen *GenConfigEntity) AddGenStore(mode, store string) {
-	fmt.Println(mode, store, "睡个好觉")
+func (gen *GenConfigEntity) AddGenStore(mode, store string) error {
+	switch mode {
+	case "module":
+		for _, item := range gen.Inputs {
+			if v, ok := item.(ModuleInputEntity); ok {
+				if v.Module == store {
+					return fmt.Errorf("store already exists")
+				}
+			}
+		}
+		gen.Inputs = append(gen.Inputs, ModuleInputEntity{
+			Module: store,
+		})
+	case "local":
+		for _, item := range gen.Inputs {
+			if v, ok := item.(LocalInputEntity); ok {
+				if v.Directory == store {
+					return fmt.Errorf("store already exists")
+				}
+			}
+		}
+		gen.Inputs = append(gen.Inputs, LocalInputEntity{
+			Directory: store,
+		})
+	}
+	return nil
 }
 
 func (gen *GenConfigEntity) RemoveGenStore(store string) {
