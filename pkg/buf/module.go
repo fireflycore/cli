ce58eb8982677fd67f24e5428cbb6092f9fafaa6
lhdht <15229684090@163.com>
2024-09-16 19:37:42 +0800

    feature: AddGenModule

--- pkg/buf/module.go
+++ pkg/buf/module.go
@@ -2,8 +2,21 @@ package buf
 
 import "fmt"
 
-func (gen *GenConfigEntity) AddGenModule(store, module string) {
-	fmt.Println(module, store)
+func (gen *GenConfigEntity) AddGenModule(store, module string) error {
+	for i, item := range gen.Inputs {
+		if v, ok := item.(ModuleInputEntity); ok {
+			if v.Module == store {
+				for _, m := range v.Types {
+					if m == module {
+						return fmt.Errorf("module already exists")
+					}
+				}
+				v.Types = append(v.Types, module)
+				gen.Inputs[i] = v
+			}
+		}
+	}
+	return nil
 }
 
 func (gen *GenConfigEntity) RemoveGenModule(module string) {
