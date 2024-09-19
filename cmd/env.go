6cb29af9a6a24aa355a90ff8fe9d537f0a76d1f4
lhdht <15229684090@163.com>
2024-09-19 10:10:50 +0800

    feature: 检查所需的环境

---
+++ cmd/env.go
@@ -0,0 +1,30 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// envCmd represents the env command
+var envCmd = &cobra.Command{
+	Use:   "env",
+	Short: "Check the required environment",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("env called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(envCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// envCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// envCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
