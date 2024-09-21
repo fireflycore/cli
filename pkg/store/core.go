bc61c898ab71e765bd4fb7a0e190b0f999097995
lhdht <15229684090@163.com>
2024-09-21 19:42:17 +0800

    fix: 废弃buf

--- pkg/store/core.go
+++ pkg/store/core.go
@@ -1,12 +1,10 @@
 package store
 
 import (
-	"github.com/fireflycore/cli/pkg/buf"
 	"github.com/fireflycore/cli/pkg/config"
 )
 
 type _CoreEntity struct {
-	Buf    *buf.CoreEntity
 	Config *config.CoreEntity
 }
 
