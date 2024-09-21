47dcddd5ccbdfe526bb90a9379103781307d3432
lhdht <15229684090@163.com>
2024-09-21 19:42:13 +0800

    fix: 废弃buf

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"github.com/fireflycore/cli/cmd"
-	"github.com/fireflycore/cli/pkg/buf"
 	"github.com/fireflycore/cli/pkg/config"
 	"github.com/fireflycore/cli/pkg/store"
 )
@@ -16,10 +15,5 @@ func main() {
 	}
 
 	store.Use.Config = cfg
-
-	if cli, err := buf.New(store.Use.Config.LocalDir); err == nil {
-		store.Use.Buf = cli
-	}
-
 	cmd.Execute()
 }
