b019889806e0090d57f3f93642b47f6e0f92c296
lhdht <15229684090@163.com>
2024-09-21 19:42:00 +0800

    fix: 废弃无效命令

--- pkg/view/problem.go
+++ pkg/view/problem.go
@@ -5,34 +5,6 @@ var CREATE_PROJECT_PROBLEM = []string{
 	"Please choose your development language.",
 }
 
-var PROTO_ADD_STORE = []string{
-	"Please select a store type.",
-	"Please enter the store address.",
-}
-
-var PROTO_ADD_MODULE = []string{
-	"Please select a store address.",
-	"Please enter the module name.",
-}
-
-var PROTO_LIST_STORE = []string{
-	"Please select a store type.",
-}
-
-var PROTO_LIST_MODULE = []string{
-	"Please select a store.",
-}
-
-var PROTO_REMOVE_STORE = []string{
-	"Please select a store type.",
-	"Please select the store you want to remove.",
-}
-
-var PROTO_REMOVE_MODULE = []string{
-	"Please select a store.",
-	"Please select the module you want to remove.",
-}
-
 var TIPS_TEXT = []string{
 	"ctrl+c or q to exit the cli.",
 	"enter confirm or next step.",
